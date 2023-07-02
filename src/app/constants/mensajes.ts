f873bc58c94fa65f897c95c5df3f80949209c9d0
jarbramirez1885 <53068006+jarbramirez1885@users.noreply.github.com>
2023-07-02 18:20:27 -0500

    proyecto completo

---
+++ src/app/constants/mensajes.ts
@@ -0,0 +1,4 @@
+export class Mensajes{
+ public static CONFIRM_CREATE_USER = "Are you sure of creating a new user?";
+ public static SUCCESS = "User created successful";
+}
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { Endpoints } from '../constants/endpoints';
+
+const baseUrl = Endpoints.END_POINTS;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(baseUrl);
+  }
+
+  get(id: any): Observable<User> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(title: any): Observable<User[]> {
+    return this.http.get<User[]>(`${baseUrl}?title=${title}`);
+  }
+}
